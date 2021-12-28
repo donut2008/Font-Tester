b482dd1ff09cb9be7eae3ec03b5b191a341fd48a
mac-andcheese <79228174+mac-andcheese@users.noreply.github.com>
2021-12-28 19:47:41 +0100

    Several new features and improvements.
    Added a button that kides/shows the settings of Font Tester
    Added an option to change the font styles

--- Hello World!/MainPage.xaml.cs
+++ Hello World!/MainPage.xaml.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Text;
+
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
 
@@ -25,6 +31,7 @@ namespace Hello_World_
         public MainPage()
         {
             this.InitializeComponent();
+            this.settingsDisabler.Visibility = Visibility.Collapsed;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -33,6 +40,44 @@ namespace Hello_World_
 
             var fonts = Microsoft.Graphics.Canvas.Text.CanvasTextFormat.GetSystemFontFamilies();
             comboFonts.ItemsSource = fonts;
+        }
+
+        private void workyoustupidass(object sender, RoutedEventArgs e)
+        {
+            bitch.Visibility = Visibility.Collapsed;
+            this.settingsActivator.Visibility = Visibility.Collapsed;
+            this.settingsDisabler.Visibility = Visibility.Visible;
+        }
+
+        private void workyoustupidass2(object sender, RoutedEventArgs e)
+        {
+            bitch.Visibility = Visibility.Visible;
+            this.settingsDisabler.Visibility = Visibility.Collapsed;
+            this.settingsActivator.Visibility = Visibility.Visible;
+        }
+
+        private void Default_FontSelected(object sender, RoutedEventArgs e)
+        {
+            padding.FontWeight = FontWeights.Normal;
+            padding.FontStyle = FontStyle.Normal;
+        }
+
+        private void Italic_FontSelected(object sender, RoutedEventArgs e)
+        {
+            padding.FontStyle = FontStyle.Italic;
+            padding.FontWeight = FontWeights.Normal;
+        }
+
+        private void Bold_FontSelected(object sender, RoutedEventArgs e)
+        {
+            padding.FontWeight = FontWeights.Bold;
+            padding.FontStyle = FontStyle.Normal;
+        }
+
+        private void BoldandItalic_FontSelected(object sender, RoutedEventArgs e)
+        {
+            padding.FontWeight = FontWeights.Bold;
+            padding.FontStyle = FontStyle.Italic;
         }
 
         private void SliderRozmiar_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
